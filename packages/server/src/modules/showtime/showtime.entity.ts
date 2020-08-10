be073a62b55c17fd87b9b284f050f651d3c2d912
Mikhail Korotkov <mkinfrared@gmail.com>
2020-08-10 23:15:08 +0300

    feat(server): Check if showtime can be inserted
    * Refactors showtime.service.ts, showtime.entity.ts
    
    Closes #31

--- packages/server/src/modules/showtime/showtime.entity.ts
+++ packages/server/src/modules/showtime/showtime.entity.ts
@@ -1,4 +1,10 @@
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn
+} from "typeorm";
 
 import { Auditorium } from "modules/auditorium/auditorium.entity";
 import { Movie } from "modules/movie/movie.entity";
@@ -11,9 +17,20 @@ export class Showtime {
   @Column({ type: "timestamptz" })
   startDate: Date;
 
+  @Column({ type: "timestamptz" })
+  endDate: Date;
+
+  @Column()
+  auditoriumId: number;
+
+  @Column()
+  movieId: number;
+
   @ManyToOne(() => Movie, (movie) => movie.id)
+  @JoinColumn({ name: "movieId" })
   movie: Movie;
 
   @ManyToOne(() => Auditorium, (auditorium) => auditorium.id)
+  @JoinColumn({ name: "auditoriumId" })
   auditorium: Auditorium;
 }
--- packages/server/src/modules/showtime/showtime.service.ts
+++ packages/server/src/modules/showtime/showtime.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import dayjs from "dayjs";
+import dayjs, { Dayjs } from "dayjs";
 import { Repository } from "typeorm";
 
-import { AuditoriumService } from "modules/auditorium/auditorium.service";
+import { Movie } from "modules/movie/movie.entity";
 import { MovieService } from "modules/movie/movie.service";
 import { ShowtimeDto } from "modules/showtime/dto/showtime.dto";
 import { Showtime } from "modules/showtime/showtime.entity";
@@ -13,30 +13,67 @@ export class ShowtimeService {
   constructor(
     @InjectRepository(Showtime)
     private readonly showtimeRepository: Repository<Showtime>,
-    private readonly movieService: MovieService,
-    private readonly auditoriumService: AuditoriumService
+    private readonly movieService: MovieService
   ) {}
 
   async create(screeningDto: ShowtimeDto) {
     const { startDate, auditoriumId, movieId } = screeningDto;
+    const date = dayjs(startDate);
     const movie = await this.movieService.getOne(movieId);
-    const auditorium = await this.auditoriumService.getOne(auditoriumId);
-    const date = dayjs(startDate).toDate();
 
     if (!movie) {
       throw new Error("Movie was not found");
     }
 
-    if (!auditorium) {
-      throw new Error("Auditorium was not found");
+    const isAvailable = await this.getTimeSlotAvailable(
+      movie,
+      auditoriumId,
+      date
+    );
+
+    if (!isAvailable) {
+      throw new Error("Slot is not available");
     }
 
     const screening = this.showtimeRepository.create({
-      movie,
-      auditorium,
-      startDate: date
+      movieId,
+      auditoriumId,
+      startDate: date.toDate(),
+      endDate: date.add(movie.runtime, "m")
     });
 
     return this.showtimeRepository.save(screening);
+  }
+
+  async getTimeSlotAvailable(movie: Movie, auditoriumId: number, date: Dayjs) {
+    const gap = 20; // minutes
+    const difference = movie.runtime + gap;
+    const prevNearestDate = date.subtract(gap, "m");
+    const nextNearestDate = date.add(difference, "m");
+
+    const showtimes = await this.showtimeRepository
+      .createQueryBuilder("showtime")
+      .select()
+      .where(
+        '"auditoriumId" = :auditoriumId AND "endDate" BETWEEN :prevDate AND :nextDate',
+        {
+          prevDate: prevNearestDate.toISOString(),
+          nextDate: nextNearestDate.toISOString(),
+          auditoriumId
+        }
+      )
+      .orWhere(
+        '"auditoriumId" = :auditoriumId AND "startDate" BETWEEN :prevDate AND :nextDate',
+        {
+          prevDate: prevNearestDate.toISOString(),
+          nextDate: nextNearestDate.toISOString(),
+          auditoriumId
+        }
+      )
+      .getMany();
+
+    const isAvailable = !showtimes.length;
+
+    return isAvailable;
   }
 }
